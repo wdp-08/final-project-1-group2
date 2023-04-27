bab56614c43f240a02f61f14e08a325faa867c92
Azkaashla <ursilaazka13@gmail.com>
2023-04-27 20:45:35 +0700

    revisi contact

--- script_contact.js
+++ script_contact.js
@@ -18,3 +18,14 @@ function moveSect(){
         }
     })
 }
+
+function kirim(){
+    url = "https://wa.me/+6281357279860";
+    var form = document.forms[0];
+    var nama = form.querySelector('[name="nama"]').value;
+    var konten = form.querySelector('[name="konten"]').value;
+    url_full = url + "?text=" + encodeURIComponent("Halo Admin, Saya " + nama + ".  ");
+    url_full = url_full + encodeURIComponent("Saya ingin memberikan kritik / saran : " + konten);
+    console.log(url_full);
+    window.open(url_full, '_blank').focus();
+}
